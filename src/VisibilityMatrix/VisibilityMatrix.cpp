8b1942fe82fdc169603565ff70607125a0cb6341
dsemeraro2 <daniele.semerarods@libero.it>
2023-03-29 14:15:45 +0200

    WIP

---
+++ src/VisibilityMatrix/VisibilityMatrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dany9 on 29/03/2023.
+//
+
+#include "VisibilityMatrix.h"
---
+++ src/VisibilityMatrix/VisibilityMatrix.h
@@ -0,0 +1,14 @@
+//
+// Created by dany9 on 29/03/2023.
+//
+
+#ifndef IOTSECURITY_VISIBILITYMATRIX_H
+#define IOTSECURITY_VISIBILITYMATRIX_H
+
+
+class VisibilityMatrix {
+
+};
+
+
+#endif //IOTSECURITY_VISIBILITYMATRIX_H
