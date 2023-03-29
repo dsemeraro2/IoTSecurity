606e019d09d900ee4a9043e09422ac00ab509d5f
dsemeraro2 <daniele.semerarods@libero.it>
2023-03-29 21:01:42 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "VisibilityMatrix/VisibilityMatrix.h"
 
 //n*m*t dove n = numero cluster, m = numero leo satellite, t = durata totale simulazione
 #define N 5
@@ -10,9 +9,6 @@ int main()
 {
     std::cout << "Ok\n";
     std::cout << "Prova matrice tridimensionale";
-
-    int matrix[N][M][T];
-    matrix[N][M][T]= newMatrix(N,M,T);
 
     return 0;
 }
