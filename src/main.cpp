dba7da0f7791579be1a21edcc0f2e45ee3a19855
dsemeraro2 <daniele.semerarods@libero.it>
2023-03-29 15:24:46 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include "VisibilityMatrix.h"
+#include "VisibilityMatrix/VisibilityMatrix.h"
 
+#define X 5
+#define Y 5
+#define Z 5
 int main()
 {
     std::cout << "Ok\n";
     std::cout << "Prova matrice tridimensionale";
 
-
-    int x = 5;
-    int y = 5;
-    int z = 5;
-
-    int matrix[x][y][z];
-    matrix[x][y][z]= newMatrix(x,y,z);
+    int matrix[X][Y][Z];
+    matrix[X][Y][Z]= newMatrix(X,Y,Z);
 
     return 0;
 }
