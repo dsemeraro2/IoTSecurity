d00e1c23f549d6d31d400c2f77ab7c4acfb05632
dsemeraro2 <daniele.semerarods@libero.it>
2023-03-31 22:04:41 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,28 @@ using namespace std;
 
 int main()
 {
-    int simulation = 24*60*60; //Durata totale della simulazione
-    int durataRivoluzione = 94*60; //Durata rivoluzione di 1 satellite
-    float timeSlotDuration = durataRivoluzione/M; //Intervallo durata visibilità di M satelliti
-    int timeSlotTotali = ceil(simulation/timeSlotDuration); //Numero totali timeslot
-    int tsDeadline = ceil(timeSlotTotali/(6));
+    int simulationTime = 24 * 60 * 60; // Durata totale della simulazione in secondi
+    int durataRivoluzione = 94 * 60; // Durata rivoluzione di 1 satellite in secondi
+    float timeSlotDuration = durataRivoluzione / M; // Intervallo durata visibilità di M satelliti in secondi
+    int timeSlotTotali = ceil(simulationTime / timeSlotDuration); // Numero totali timeslot
+    int simulationTimeSlotDeadline = 6 * 60 * 60; // Durata totale di un servizio in secondi
+
+    int tsDeadline = ceil(simulationTimeSlotDeadline / timeSlotDuration); // Timeslot entro cui eseguire un servizio
 
     cout<<"\ntimeSlotTotali:"<<timeSlotTotali;
-    cout<<"\nnumberTimeSlot:"<<tsDeadline;
+    cout<<"\ntsDeadline:"<<tsDeadline;
+
+    /*
+    std::vector<Request> requests;
+
+    for (int i = 0; i < 10; i++) {
+        requests.insert(requests.end(), {i, 0, 1, -1});
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        std::cout<<"\nRequest: " << requests[i].getId();
+    }
 
     /*
     VisibilityMatrix matrix(timeSlotTotali,N,M);
