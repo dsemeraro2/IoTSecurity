7710d0b3a2ac40faf7ad3aede7ebafcd9126605c
dsemeraro2 <daniele.semerarods@libero.it>
2023-03-31 14:21:34 +0200

    Added allocation services matrix

---
+++ src/AllocationServicesMatrix/AllocationServicesMatrix.cpp
@@ -0,0 +1,65 @@
+#include "AllocationServicesMatrix.h"
+
+AllocationServicesMatrix::AllocationServicesMatrix(int t, int n, int m) {
+    this->tMax = t;
+    this->nMax = n;
+    this->mMax = m;
+    // Allocazione dinamica della matrice tramite il metodo new
+    data = new int **[t];
+    for (int i = 0; i < t; i++) {
+        data[i] = new int *[n];
+        for (int j = 0; j < n; j++) {
+            data[i][j] = new int[m];
+            for (int k = 0; k < m; k++) {
+                data[i][j][k] = 0;
+            }
+        }
+    }
+}
+
+int AllocationServicesMatrix::getNMax() const {
+    return nMax;
+}
+
+int AllocationServicesMatrix::getMMax() const {
+    return mMax;
+}
+
+int AllocationServicesMatrix::getTMax() const {
+    return tMax;
+}
+
+// Set a value a un'elemento della matrice alle coordinate (tMax,nMax,mMax)
+void AllocationServicesMatrix::setValue(int t, int n, int m, int value) {
+    // Controlla se gli indici sono validi
+    if (t < 0 || t >= tMax || n < 0 || n >= nMax || m < 0 || m >= mMax) {
+        throw std::out_of_range("Indici fuori dal range");
+    }
+    // Assegna il valore alla posizione specificata
+    data[t][n][m] = value;
+}
+
+void AllocationServicesMatrix::showMatrix() {
+    for (int i = 0; i < tMax; i++) {
+        std::cout << "\nT:" << i;
+        for (int j = 0; j < nMax; j++) {
+            std::cout << "\n";
+            for (int k = 0; k < mMax; k++) {
+                std::cout << data[i][j][k] << " ";
+            }
+        }
+    }
+}
+
+void AllocationServicesMatrix::initialize() {
+
+    for (int i = 0; i < tMax; i++) {
+        for (int j = 0; j < nMax; j++) {
+            //for (int k = 0; k < mMax; k++) {
+            int k = (j + i) % mMax;
+            data[i][j][k] = 1;
+            //}
+        }
+    }
+
+}
---
+++ src/AllocationServicesMatrix/AllocationServicesMatrix.h
@@ -0,0 +1,35 @@
+#ifndef IOTSECURITY_ALLOCATIONSERVICESMATRIX_H
+#define IOTSECURITY_ALLOCATIONSERVICESMATRIX_H
+
+#include <iostream>
+
+class AllocationServicesMatrix {
+private:
+    int tMax, nMax, mMax;
+    int*** data;
+public:
+    AllocationServicesMatrix(int t, int n, int m);
+
+    // Restituisce l'elemento della matrice alle coordinate (tMax,nMax,mMax)
+    int& operator()(int t, int n, int m) {
+        return data[t][n][m];
+    }
+
+    int getNMax() const;
+
+    int getMMax() const;
+
+    int getTMax() const;
+
+    // Set a value a un'elemento della matrice alle coordinate (tMax,nMax,mMax)
+    void setValue (int t, int n, int m, int value);
+
+    // Mostra tutta la matrice
+    void showMatrix();
+
+    void initialize();
+
+};
+
+
+#endif //IOTSECURITY_ALLOCATIONSERVICESMATRIX_H
