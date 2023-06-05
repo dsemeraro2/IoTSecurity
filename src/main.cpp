2a520c9e377b51d20289711a018f6c7f3bfc6b7e
dsemeraro2 <daniele.semerarods@liberolit>
2023-06-05 21:07:40 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@ using namespace std;
 
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <filesystem>
 #include "VisibilityMatrix/VisibilityMatrix.h"
 #include "Request/Request.h"
 #include "Simulation/Simulation.h"
@@ -11,6 +13,47 @@ using namespace std;
 
 Solution generateSolution() {
     return Solution(timeSlotTotali);
+}
+void saveSolutionToFile (const Solution &solution, const std::string &folderPath, const std::string &fileName, int constellationIndex) {
+    std::filesystem::create_directory(folderPath);
+
+    std::string filename = folderPath + fileName + std::to_string(constellationIndex) + ".txt";
+    std::ofstream file(filename);
+
+    if (file.is_open()) {
+        // Salva le informazioni generali della soluzione
+        //file << "Time Slot: " << solution.timeSlot << "\n";
+        file << "f: " << solution.f << "\n";
+        file << "Constellations: " << solution.constellations.size() << "\n\n";
+
+        // Salva le informazioni di ogni costellazione
+        for (const Constellation& constellation : solution.constellations) {
+            //file << "Constellation (mMax = " << constellation.mMax << ")\n";
+            file << "Satellites: " << constellation.satellaties.size() << "\n";
+
+            // Salva le informazioni di ogni satellite nella costellazione
+            for (const Satellite& satellite : constellation.satellaties) {
+                file << "Satellite (id = " << satellite.getId() << ", cpu = " << satellite.getCpu()
+                     << ", ram = " << satellite.getRam() << ")\n";
+                file << "Services: " << satellite.getServices().size();
+
+                // Salva le informazioni di ogni servizio nel satellite
+                for (const Service& service : satellite.getServices()) {
+                    file << "Service (id = " << service.getId() << ", cpuUsed = " << service.getCpuUsed()
+                         << ", ramUsed = " << service.getRamUsed() << ")\n";
+                }
+
+                file << "\n";
+            }
+
+            file << "\n";
+        }
+
+        file.close();
+        std::cout << "Soluzione salvata correttamente su file: " << filename << "\n";
+    } else {
+        std::cout << "Impossibile aprire il file: " << filename << "\n";
+    }
 }
 
 int main() {
@@ -43,13 +86,22 @@ int main() {
 
     std::cout << "Funzione obiettivo: " << solution.f << "\n";
 
-    // Algoritmo di ottimizzazione
+    //Salvataggio su file prima dell'ottimizzazione
+    for (int i = 0; i < solution.constellations.size(); ++i) {
+        const Constellation& constellation = solution.constellations[i];
+        saveSolutionToFile(solution, "./solution_constellation_old", "/solution_constellation_old", i);
+    }
 
+    // Algoritmo di ottimizzazione
     std::cout << "Ottimizzazione:\n";
     TabuSearch tabuSearch = TabuSearch(T, M, services, requests, solution, visibilityMatrix, allocationServicesMatrix);
     tabuSearch.optimizationTabuSearch(0, T);
 
-    //TODO SALVATAGGIO SU TXT LA CONFIGURAZIONE di costellation
+    //Salvataggio su file fopo l'ottimizzazione
+    for (int i = 0; i < solution.constellations.size(); ++i) {
+        const Constellation& constellation = solution.constellations[i];
+        saveSolutionToFile(solution, "./solution_constellation_new", "/solution_constellation_new", i);
+    }
 
     return 0;
 }
